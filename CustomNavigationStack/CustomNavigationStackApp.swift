0eaf271e989be7d5e28ce5886def39f65e1d6663
Natalia Zhabitskaya <n.zhabitskaya@gmail.com>
2024-11-27 02:27:30 -0800

    Initial Commit

---
+++ CustomNavigationStack/CustomNavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationStackApp.swift
+//  CustomNavigationStack
+//
+//  Created by Natalia on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CustomNavigationStackTests/CustomNavigationStackTests.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavigationStackTests.swift
+//  CustomNavigationStackTests
+//
+//  Created by Natalia on 11/27/24.
+//
+
+import Testing
+@testable import CustomNavigationStack
+
+struct CustomNavigationStackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
